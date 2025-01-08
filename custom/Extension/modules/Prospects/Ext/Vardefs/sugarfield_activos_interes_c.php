948a83e2e8ee6c18415487b724325c9eea288053
IRVING DELGADO <irving.delgado@tactos.com.mx>
2025-01-08 14:18:32 -0600

    ID: CRM - Mejoras en la trazabilidad del registro en Público Objetivo Ticket#35-06012025

--- custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_activos_interes_c.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_activos_interes_c.php
@@ -1,8 +1,7 @@
 <?php
- // created: 2025-01-06 19:56:56
+ // created: 2025-01-07 18:57:01
 $dictionary['Prospect']['fields']['activos_interes_c']['labelValue']='Activos de interés ';
 $dictionary['Prospect']['fields']['activos_interes_c']['dependency']='';
-$dictionary['Prospect']['fields']['activos_interes_c']['required_formula']='';
 $dictionary['Prospect']['fields']['activos_interes_c']['readonly_formula']='';
 $dictionary['Prospect']['fields']['activos_interes_c']['visibility_grid']=array (
 );
--- custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_potencial_cierre_c.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_potencial_cierre_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2025-01-06 20:00:03
+ // created: 2025-01-07 18:57:52
 $dictionary['Prospect']['fields']['potencial_cierre_c']['labelValue']='Potencial de cierre';
 $dictionary['Prospect']['fields']['potencial_cierre_c']['full_text_search']=array (
   'enabled' => '0',
@@ -8,7 +8,6 @@ $dictionary['Prospect']['fields']['potencial_cierre_c']['full_text_search']=arra
 );
 $dictionary['Prospect']['fields']['potencial_cierre_c']['enforced']='';
 $dictionary['Prospect']['fields']['potencial_cierre_c']['dependency']='';
-$dictionary['Prospect']['fields']['potencial_cierre_c']['required_formula']='';
 $dictionary['Prospect']['fields']['potencial_cierre_c']['readonly_formula']='';
 
  ?>
